063c04a6ad1eed0b86931c5b05443e89cfd7a8be
marisaazevedo <marisapsazevedo@gmail.com>
2023-10-11 11:10:39 +0100

    login and register interface

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
-const app = document.getElementById('app'); 
-
 const homePage = document.getElementById('homePage');
 const gameBoard = document.getElementById('gameBoard');
 const rulesSection = document.getElementById('rules');
-const leaderboard = document.getElementById('Classifications'); 
+const leaderboard = document.getElementById('Classifications');
 const gameSettings = document.getElementById('game-Settings');
-
 const boardContainer = document.getElementById('board');
+const loginRegisterSection = document.getElementById('login-register');
+const menuContainer = document.getElementById('menuContainer')
 
 function SetGameSettings() {
     gameBoard.style.display = 'none';
@@ -14,31 +13,61 @@ function SetGameSettings() {
     leaderboard.style.display = 'none';
     homePage.style.display = 'block';
     gameSettings.style.display = 'block';
+    menuContainer.style.display = 'block';
 }
 
 function startGame() {
-    homePage.style.display = 'none';
+    homePage.style.display = 'block';
     rulesSection.style.display = 'none';
     leaderboard.style.display = 'none';
     gameBoard.style.display = 'block';
     gameSettings.style.display = 'none';
+    menuContainer.style.display = 'none';
     render(boardContainer);
 }
 
 function showRules() {
-    homePage.style.display = 'none';
+    homePage.style.display = 'block';
     gameBoard.style.display = 'none';
     gameSettings.style.display = 'none';
     rulesSection.style.display = 'block';
 }
 
 function classification() {
-    homePage.style.display = 'none';
+    homePage.style.display = 'block';
     gameBoard.style.display = 'none';
     rulesSection.style.display = 'none';
     gameSettings.style.display = 'none';
     leaderboard.style.display = 'block';
-    
+}
+
+// Função de login
+function login() {
+    const loginUser = document.getElementById('loginUser').value;
+    const loginPassword = document.getElementById('loginPassword').value;
+
+    // Verifique as credenciais (coloque sua lógica de verificação aqui)
+    // Exemplo: Você pode verificar as credenciais em um servidor ou usar um objeto de usuário simulado.
+
+    if (loginUser === 'guest' && loginPassword === 'guest') {
+        // Login bem-sucedido, oculte a seção de login/registro e mostre o menu.
+        menuContainer.style.display = 'block';
+        homePage.style.display = 'block';
+        loginRegisterSection.style.display = 'none';
+    } else {
+        alert('Login failed. Please check your credentials.');
+    }
+}
+
+// Função de registro
+function register() {
+    const registerUser = document.getElementById('registerUser').value;
+    const registerPassword = document.getElementById('registerPassword').value;
+
+    // Registre o usuário (coloque sua lógica de registro aqui)
+    // Exemplo: Você pode enviar os dados de registro para um servidor.
+
+    alert('User registered successfully. You can now log in.');
 }
 
 const board = Array(30).fill('');
@@ -71,8 +100,6 @@ function render(boardContainer) {
     });
 }
 
-
-
 function handleCellClick(index) {
     if (board[index] === '' && !checkWinner()) {
         board[index] = currentPlayer;
@@ -83,10 +110,6 @@ function handleCellClick(index) {
         }
     }
 }
-
-
-
-
 
 function checkWinner() {
     // Check for 3 in a row horizontally
