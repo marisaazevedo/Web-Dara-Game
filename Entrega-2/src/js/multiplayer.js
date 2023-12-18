920f3a9269d2f80bcd95e1f50ed4021c1e580924
franciscoribeiro2003 <up202104797@up.pt>
2023-12-18 01:39:34 +0000

    phase2

--- Entrega-2/src/js/multiplayer.js
+++ Entrega-2/src/js/multiplayer.js
@@ -5,8 +5,9 @@ var username, pass;
 var gameID;
 var gameStatus;
 var playerNumber;
-// var playerTurn;
-// var rows, columns;
+var playerColor;
+var turn;
+var step;
 var row, column;
 
 
@@ -61,30 +62,55 @@ async function updategame(event) {
     if (json === null) {
         console.log("Error updating game");
     }
-    else if (json !== null && "turn" in json) {
+    else if (json !== null) {
         try {
             if ("error" in json){
                 console.log("Update in error");
+                if (json.error == "not your piece"){
+                    //drawBoardServer();
+                    console.log("Error: not your piece");
+                }
             }
             else if ("board" in json){
                 board = json.board;
-                console.log(board);
                 Promise.resolve(json.board).then(() => {
                     countingPiecesServer();
                     countingpieces();
                 });
                 if (json.phase == "drop") phase = 1;
-                else phase = 2;
+                else {
+                    phase = 2;
+                    step = json.step;
+                }
                 players = Object.values(json.players);
                 turn = json.turn;
-                
+                playerColor=json.players[username];
                 if (json.players[username] == "white") playerNumber = 1;
                 else playerNumber = 2;
                 
                 if (turn == username) currentPlayer = playerNumber;
                 else currentPlayer = 3 - playerNumber;
+                if ("winner" in json) {
+                    if (json.winner == username) {
+                        displayMessage("Congratulations! You won!");
+                    }
+                    else {
+                        displayMessage("You lost! Better luck next time!");
+                    }
+                    eventSource.close();
+                }
 
                 drawBoardServer();
+
+            }
+            else if ("winner" in json){
+                if (json.winner == username) {
+                    displayMessage("Congratulations! You won!");
+                }
+                else {
+                    displayMessage("You lost! Better luck next time!");
+                }
+                eventSource.close();
             }
         }
         catch {
@@ -128,46 +154,41 @@ async function drawBoardServer () {
             } else if (board[row][col] === 'black') {
                 hole.style.backgroundColor = p2; // Player 2 color
             }
-
-            if (phase === 2) {
-                if(checkForWinner() == 1) {
-                gameOver(1);
-                return;
-                }
-                else if(checkForWinner() == 2) {
-                    gameOver(2);
-                    return;
+            try {
+                if (boardAvailable[row][col] === 'available') {
+                    hole.style.backgroundColor = 'grey';
+                    boardAvailable[row][col] = 'empty';
                 }
             }
+            catch {}
 
             // Add event listeners based on the current phase
-            if ((player1Pieces > 0 || player2Pieces > 0) && phase === 1 && playerNumber === currentPlayer) {
+            if (phase === 1 && playerNumber === currentPlayer) {
                 displayMessage('Your turn! Place a piece on the board.');
                 hole.addEventListener('click', async () => await makeMovePhase1Server(row, col));
             }
+            if (phase === 2 && playerNumber === currentPlayer) { 
+                displayMessage('Your turn! Move a piece.');        
+                hole.addEventListener('click',async () => await makeMovePhase2Server(row,col));
+            }
+
             if (playerNumber !== currentPlayer) {
                 displayMessage('Waiting for other player to play!');
-            }
-            if(player1Pieces === 0 && player2Pieces === 0 && phase === 1){
-                displayMessage('Phase 1 completed! Now proceed to Phase 2.');
-                phase = 2;
-            }
-            if (phase === 2 && removeFlag === false) {
-                hole.addEventListener('click',async () => await makeMovePhase2(row,col));
             }
 
             boardElement.appendChild(hole);
         }
         boardElement.appendChild(document.createElement('br'));
     }
+
 }
 
 async function makeMovePhase1Server(row, col) {
     // Check if placing the piece would form more than 4 in a row in a line or column
     const { rows, cols } = boardSizes[boardSize];
 
-    const horizontalCount = countSameColorInRow(board, col, row, cols, currentPlayer);
-    const verticalCount = countSameColorInColumn(board, col, row, rows, cols, currentPlayer);
+    const horizontalCount = countSameColorInRowServer(board, col, row, cols, playerColor);
+    const verticalCount = countSameColorInColumnServer(board, col, row, rows,  playerColor);
     
     if (board[row][col] !== 'empty') {
         displayMessage('Invalid move. Please choose an empty hole.');
@@ -177,17 +198,16 @@ async function makeMovePhase1Server(row, col) {
 
     else if (horizontalCount || verticalCount ) {
         displayMessage('Invalid move. Placing this piece would form more than 3 in a row.');
-        return;
+        body = {"nick": username, "password": pass, "game": gameID, "move": {row: row, column: col}};
+        notify(body);
     }
 
     else if (currentPlayer === 1 && player1Pieces > 0 && playerNumber === currentPlayer) {
         board[row] [col] = 'white';
-        //countingPiecesServer();
         body = {"nick": username, "password": pass, "game": gameID, "move": {row: row, column: col}};
         notify(body);
     } else if (currentPlayer === 2 && player2Pieces > 0 && playerNumber === currentPlayer) {
         board[row][col] = 'black';
-        //countingPiecesServer();
         body = {"nick": username, "password": pass, "game": gameID, "move": {"row": row, "column": col}};
         notify(body);
     }
@@ -203,7 +223,6 @@ async function makeMovePhase1Server(row, col) {
 }
 
 function countingPiecesServer(){
-    console.log("------countingPiecesServer------\n" + board);
     player1PiecesCounter = 0;
     player2PiecesCounter = 0;
     for(let i = 0; i < board.length; i++){
@@ -214,6 +233,100 @@ function countingPiecesServer(){
     }
     player1Pieces = 12 - player1PiecesCounter;
     player2Pieces = 12 - player2PiecesCounter;
-    console.log("player1Pieces: " + player1Pieces + " player1PiecesCounter: " + player1PiecesCounter);
-    console.log("player2Pieces: " + player2Pieces + " player2PiecesCounter: " + player2PiecesCounter);
+}
+
+var boardAvailable = [];
+function makeMovePhase2Server (row, col) {
+    const { rows, cols } = boardSizes[boardSize];
+    if (step === 'from' && turn === username && board[row][col] === playerColor){
+        let availableMoves = availableIndexsForPieceServer(row, col);
+        boardAvailable = board;
+        availableMoves.forEach(({ row, col }) => {
+            boardAvailable[row][col] = 'available';
+        });
+
+    }
+    else if (step === 'from' && turn === username && board[row][col] !== playerColor){
+        displayMessage('Invalid move. Please choose one of your pieces.');
+    }
+    if (step === 'take'){
+        displayMessage('Remove an opponent piece.');
+        if (board[row][col] === turn){
+            displayMessage('Invalid move. Please choose an opponent piece.');
+        }
+    }
+    notify({"nick": username, "password": pass, "game": gameID, "move": {"row": row, "column": col}});
+}
+
+function availableIndexsForPieceServer (row, col){
+    let availableIndexs = [];
+    if (row > 0 && board[row - 1][col] === 'empty') availableIndexs.push({ row: row - 1, col });
+    if (row < board.length - 1 && board[row + 1][col] === 'empty') availableIndexs.push({ row: row + 1, col });
+    if (col > 0 && board[row][col - 1] === 'empty') availableIndexs.push({ row, col: col - 1 });
+    if (col < board[row].length - 1 && board[row][col + 1] === 'empty') availableIndexs.push({ row, col: col + 1 });
+    return availableIndexs;
+}
+
+function countSameColorInRowServer (board, col, row, cols, color) {
+    let countLeft = 0;
+    let countRight = 0;
+
+    
+    let currentc = col;
+    try {
+        currentc = col - 1;
+        while (currentc >= 0 && board[row][currentc] === color) {
+            if (currentc !== col) countLeft++;
+            else break;
+            currentc--;
+        }
+    }
+    catch { }
+    if (countLeft >= 2) return true;
+
+    try {
+        currentc = col + 1;
+        while (currentc < cols && board[row][currentc] === color) {
+            if (currentc !== col) countRight++;
+            else break;
+            currentc++;
+        }
+    }
+    catch { }
+    if (countRight >= 2) return true; 
+        
+        
+    if (countLeft + countRight>= 3) return true;
+    return false;
+}
+
+function countSameColorInColumnServer (board, col, row, rows, color) {
+    let countUp = 0;
+    let countDown = 0;
+
+    let currentr = row;
+    try {
+        currentr = row - 1;
+        while (currentr >= 0 && board[currentr][col] === color) {
+            if (currentr !== row) countUp++;
+            else break;
+            currentr--;
+        }
+    }
+    catch { }
+    if (countUp >= 2) return true;
+
+    try {
+        currentr = row + 1;
+        while (currentr < rows && board[currentr][col] === color) {
+            if (currentr !== row) countDown++;
+            else break;
+            currentr++;
+        }
+    }
+    catch { }
+    if (countDown >= 2) return true;
+
+    if (countUp + countDown >= 3) return true;
+    return false;
 }
